371d467bc56b5d2dc828b855773bfada7fd1fb48
Ryan Ata <ataryan47@gmail.com>
2024-01-26 00:24:48 -0500

    added bearer token to all two requests

--- frontend/src/pages/DocumentationPage.tsx
+++ frontend/src/pages/DocumentationPage.tsx
@@ -1,23 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
+import { getAuth } from "firebase/auth";
 import Sidebar from '@/components/sidebar';
 import Editor from '@/components/editor';
-
-// Define a fetch function
-const fetchDocs = async (id: string) => {
-    console.log(id);
-    const response = await fetch(`${process.env.NODE_ENV === 'development' ? '/file-docs/' : 'https://notebites.app/file-docs/'}${id}`);
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-};
 
 const DocumentationPage: React.FC = () => {
     const [selectedItem, setSelectedItem] = useState<string | null>(null);
     const [documentation, setDocumentation] = useState<string | null>(null);
     const { id } = useParams<{ id: string }>(); // Get the id from the URL
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    const fetchDocs = async (id: string) => {
+        const token = await user?.getIdToken();
+        console.log(token);
+        const response = await fetch(`${process.env.NODE_ENV === 'development' ? '/file-docs/' : 'https://notebites.app/file-docs/'}${id}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        if (!response.ok) {
+        throw new Error('Network response was not ok');
+        }
+        return response.json();
+    };
+
     const { data: doc, error, isLoading, refetch } = useQuery(['fileDocs', id], () => fetchDocs(id || ''), { enabled: !!id, staleTime: Infinity });    
     useEffect(() => {
         const intervalId = setInterval(() => {
